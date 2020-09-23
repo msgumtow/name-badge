c3c09e9649e76aa1f45f3ff582e1ac2c6213993e
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-09-23 14:26:14 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+    basic.showString("YOUR NAME GOES HERE")
+    basic.pause(5000)
+})
