435a26b91ef0dd9a5b730858004dbb7b5e5c105d
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-09-23 14:35:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 input.onButtonPressed(Button.A, function () {
 	
 })
+input.onButtonPressed(Button.AB, function () {
+	
+})
 input.onButtonPressed(Button.B, function () {
 	
 })
-basic.forever(function () {
-    basic.showString("YOUR NAME GOES HERE")
-    basic.pause(5000)
+input.onGesture(Gesture.Shake, function () {
+	
 })
